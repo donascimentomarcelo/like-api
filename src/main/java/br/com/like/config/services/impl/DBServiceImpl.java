34df29f883ebd1cf8f7b7c563b2dcb89cfe3b35c
Marcelo <marcelojunin2010@hotmail.com>
2021-03-01 13:07:40 -0300

    fix data on DBServiceImpl

--- src/main/java/br/com/like/config/services/impl/DBServiceImpl.java
+++ src/main/java/br/com/like/config/services/impl/DBServiceImpl.java
@@ -68,18 +68,18 @@ public class DBServiceImpl implements DBService {
 
         categoryRepository.saveAll(Arrays.asList(c1, c2, c3, c4));
 
-        Product p1 = new Product(1L, "Computador", 2000.00, desc, 20, c1);
-        Product p2 = new Product(2L,"Impressora", 800.00, desc, 10, c1);
-        Product p3 = new Product(3L,"Mouse", 80.00, desc, 80, c1);
+        Product p1 = new Product(1L, "Computador", 2000.00, desc, 20, c4);
+        Product p2 = new Product(2L,"Impressora", 800.00, desc, 10, c4);
+        Product p3 = new Product(3L,"Mouse", 80.00, desc, 80, c4);
         Product p4 = new Product(4L,"Mesa de escritorio", 300.00, desc, 40, c2);
         Product p5 = new Product(5L,"Toalha", 30.00, desc, 13, c2);
         Product p6 = new Product(6L,"Colcha", 200.00, desc, 4, c2);
-        Product p7 = new Product(7L,"TV true color", 1200.00, desc, 7, c3);
-        Product p8 = new Product(8L,"Rocadeira", 800.00, desc, 20, c3);
+        Product p7 = new Product(7L,"TV", 1200.00, desc, 7, c3);
+        Product p8 = new Product(8L,"Roçadeira", 800.00, desc, 20, c3);
         Product p9 = new Product(9L,"Abajour", 100.00, desc, 20, c3);
-        Product p10 = new Product(10L,"Pendente", 180.00, desc, 10, c4);
-        Product p11 = new Product(11L,"Cama", 190.00, desc, 5, c4);
-        Product p12 = new Product(12L,"Travesseiro", 280.00, desc, 14, c4);
+        Product p10 = new Product(10L,"Pendente", 180.00, desc, 10, c1);
+        Product p11 = new Product(11L,"Cama", 190.00, desc, 5, c1);
+        Product p12 = new Product(12L,"Travesseiro", 280.00, desc, 14, c1);
 
         Comment cm1 = new Comment(1L, 5, "Good", desc, null, p12);
         Comment cm2 = new Comment(2L, 2, "Bad", desc, null, p12);
